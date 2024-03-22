66574523d358311e13ebf9559c645824a4910267
Duy Quoc Anh Nguyen <boyporo2017@gmail.com>
2024-03-23 01:21:17 +1030

    Navigation bar demo
    this is a demo to make sure we are not behind, interface and any extra features can be fixed later on.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { Link, Outlet } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+              <div className="container-fluid">
+                  
+                  <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                      <span className="navbar-toggler-icon"></span>
+                  </button>
+                  <div className="collapse navbar-collapse" id="navbarNav">
+                      <div className="navbar-nav">
+                          <Link className="nav-link active" to="Home" >Home</Link>
+                          <Link className="nav-link" to="About" >About</Link>
+                          <Link className="nav-link" to="SignIn" >SignIn</Link>
+                          <Link className="nav-link" to="Register" >Register</Link>
+                      </div>
+                  </div>
+              </div>
+          </nav>
+          <Outlet />
+      </div>
   );
 }
 
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home, About, SignIn, Register } from './routes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App />}>
+                    <Route path="Home" element={<Home />} />
+                    <Route path="About" element={<About />} />
+                    <Route path="SignIn" element={<SignIn />} />
+                    <Route path="Register" element={<Register />} />
+                    <Route path="" element={<Home />} />
+                    <Route path="*" element={<Home />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
   </React.StrictMode>
 );
 
