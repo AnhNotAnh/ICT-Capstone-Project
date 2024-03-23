c51010311860bef5135dfffd80a2cd8e6f2be51f
rijin1 <rijyy001@mymail.unisa.edu.au>
2024-03-23 23:28:23 +1030

    Made the main heading appear in the top section of the page, adjusted the font to make it more visible, adjusted the background to fit the page well

--- my-app/src/routes/Home.js
+++ my-app/src/routes/Home.js
@@ -6,12 +6,12 @@ const Home = () => {
     return (
         <div className="home-container">
           <div className="centered-content">
-            <h2 style={{ position: 'relative', zIndex: 1 }}>Medical Sonography Logbook</h2>
+            <h2 style={{ position: 'relative', zIndex: 1,color:'black', fontWeight:'bold', fontFamily:'Times New Roman', top:'-200%' }}>Medical Sonography Logbook</h2>
             <img
               src={image}
               alt=""
               className="center-image"
-              style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', maxWidth: '80vw', height: 'auto', zIndex: 0 }}
+              style={{ position: 'absolute', top: '110%', left: '50%', transform: 'translate(-50%, -50%)', maxWidth: '', height: 'auto', zIndex: 0 }}
             />
           </div>
         </div>
