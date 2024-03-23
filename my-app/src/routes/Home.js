b2a981fc0cfa8db799334c099c752f1d8b7d6a6f
rijin1 <rijyy001@mymail.unisa.edu.au>
2024-03-23 20:32:22 +1030

    added a css style in index.css to adjust the image size and added image folder and added 2 unisa images within them and currently setting up the home page in a cleaner manner

--- my-app/src/routes/Home.js
+++ my-app/src/routes/Home.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import image from '../Images/bkg2.webp';
+
 
 const Home = () => {
-return (
-    <div>
-        <h2>Home</h2>
-    </div>
-)
+    return (
+        <div className="home-container">
+          <div className="centered-content">
+            <h2 style={{ position: 'relative', zIndex: 1 }}>Medical Sonography Logbook</h2>
+            <img
+              src={image}
+              alt=""
+              className="center-image"
+              style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', maxWidth: '80vw', height: 'auto', zIndex: 0 }}
+            />
+          </div>
+        </div>
+      );
 }
 
 export default Home
