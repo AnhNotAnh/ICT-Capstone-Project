ac6c328f96ef2f5caa113db8f45f6a7e8b87f53f
Duy Quoc Anh Nguyen <boyporo2017@gmail.com>
2024-03-24 14:45:25 +1030

    Delete href

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -18,7 +18,7 @@ function App() {
                           <Link className="nav-link active" to="Home" >Home</Link>
                           <Link className="nav-link" to="About" >About</Link>
                           <div className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <a className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Login
                             </a>
                             <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
