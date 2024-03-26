436f6ee06785eccd8f4985c55b123e3e55bcd686
rijin1 <rijyy001@mymail.unisa.edu.au>
2024-03-26 12:55:08 +1030

    Changed the image position to top 63.3 % as this makes the image stay alligned. Added a new unisa logo instead of the previous one as per the unisa marketting guide , added the same blue color hex format onto the navbar background as per unisa home page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,12 @@
-import UniSALogo from './unisa_stem_logo.png';
+import UniSALogo from '../src/Images/unisa logo white.webp';
 import './App.css';
 import { Link, Outlet } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <nav className="navbar navbar-expand-lg navbar-light custom-navbar">
               <div className="container-fluid">
                   <Link className="navbar-brand" to="/">
                       <img src={UniSALogo} alt="Logo" style={{ width: "100px", height: "auto" }} />
@@ -15,10 +16,10 @@ function App() {
                   </button>
                   <div className="collapse navbar-collapse" id="navbarNav">
                       <div className="navbar-nav">
-                          <Link className="nav-link active" to="Home" >Home</Link>
-                          <Link className="nav-link" to="About" >About</Link>
+                          <Link className="nav-link active" to="Home" style={{color:'white'}}>Home</Link>
+                          <Link className="nav-link" to="About" style={{color:'white'}}>About</Link>
                           <div className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <a className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" style={{color:'white'}}>
                                 Login
                             </a>
                             <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
--- my-app/src/routes/Home.js
+++ my-app/src/routes/Home.js
@@ -6,12 +6,12 @@ const Home = () => {
     return (
         <div className="home-container">
           <div className="centered-content">
-            <h2 style={{ position: 'relative', zIndex: 1,color:'white', fontWeight:'bold', fontFamily:'Times New Roman', top:'-200%' }}>Medical Sonography Logbook</h2>
+            <h2 style={{ position: 'relative', zIndex: 1,color:'white', fontWeight:'bold', fontFamily:'helvetica', fontSize: '600%' ,top:'0%' }}>Medical Sonography<br/> Logbook</h2>
             <img
               src={image}
               alt=""
               className="center-image"
-              style={{ position: 'absolute', top: '58.5%', left: '50%', transform: 'translate(-50%, -50%)', maxWidth: '70%', height: 'auto', zIndex: 0 }}
+              style={{ position: 'absolute', top: '65.6%', left: '50%', transform: 'translate(-50%, -50%)', maxWidth: '70%', height: 'auto', zIndex: 0 }}
             />
           </div>
         </div>
