4894c2188c219335e29b7aff4d92c0a299c1a65e
Duy Quoc Anh Nguyen <boyporo2017@gmail.com>
2024-03-29 21:27:56 +1030

    Change the input form to medium size
    To make it similar to sign in page, i change the form of input to medium size.
    I delete the tick box for agree term as not really necessary.

--- my-app/src/routes/Register.js
+++ my-app/src/routes/Register.js
@@ -1,8 +1,9 @@
 import React, {useState, useEffect} from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   
-  const [role, setRole] = useState("");
+  const [role, setRole] = useState("Student");
   const [fName, setfName] = useState("");
   const [lName, setlName] = useState("");
   const [phone, setPhoneNumb] = useState("");
@@ -24,7 +25,7 @@ const Register = () => {
           
                 <h3 className="fw-normal mb-4 mt-4" style={{color:"black"}}><b>Registration Form</b></h3>
                 <div className="p-3 mb-4 pb-2">
-                  <select defaultValue="Student" className='form-select' onSelect={(e) => (setRole(e.target.value))}>
+                  <select defaultValue={role} className='form-select' onSelect={(e) => (setRole(e.target.value))}>
                     <option value="Student">Select role (default role is student) </option>
                     <option value="Student">Student</option>
                     <option value="Supervisor">Supervisor</option>
@@ -35,7 +36,7 @@ const Register = () => {
                   <div className="col-md-6 mb-4 pb-2">
                     <div className="form-outline">
                       <label style={{color:"black"}} className="form-label label-style">First Name</label>
-                      <input type="text" className="form-control form-control-lg" value={fName} placeholder="First Name"
+                      <input type="text" className="form-control form-control-md" value={fName} placeholder="First Name"
                       onChange={(e)=>(setfName(e.target.value))}>
                       </input>
                     </div>
@@ -43,7 +44,7 @@ const Register = () => {
                   <div className="col-md-6 mb-4 pb-2">
                     <div className="form-outline">
                       <label style={{color:"black"}} className="form-label label-style">Last Name</label>
-                      <input type="text" className="form-control form-control-lg" value={lName} placeholder="Last Name"
+                      <input type="text" className="form-control form-control-md" value={lName} placeholder="Last Name"
                       onChange={(e)=>(setlName(e.target.value))}></input>
                     </div>
                   </div>
@@ -52,7 +53,7 @@ const Register = () => {
                 <div className="p-3 mb-4 pb-2">
                   <div className='form-outline form-white'>
                     <label style={{color:"black"}} className="form-label label-style">Street Address</label>
-                    <input type="text" className="form-control form-control-lg" value={street} placeholder="Street Address"
+                    <input type="text" className="form-control form-control-md" value={street} placeholder="Street Address"
                       onChange={(e)=>(setStreet(e.target.value))}></input>
                   </div>
                 </div>
@@ -60,14 +61,14 @@ const Register = () => {
                   <div className='col-md-5 mb-4 pb-2'>
                     <div className="form-outline form-white">
                       <label style={{color:"black"}} className="form-label label-style">Post Code</label>
-                      <input type="text" className="form-control form-control-lg" value={postCode} placeholder="Post Code"
+                      <input type="text" className="form-control form-control-md" value={postCode} placeholder="Post Code"
                       onChange={(e)=>(setPostCode(e.target.value))}></input>
                     </div>  
                   </div> 
                   <div className='col-md-7 mb-4 pb-2'>
                     <div className="form-outline form-white">
                       <label style={{color:"black"}} className="form-label label-style">Suburb</label>
-                      <input type="text" className="form-control form-control-lg" value={suburb}  placeholder="Suburb"
+                      <input type="text" className="form-control form-control-md" value={suburb}  placeholder="Suburb"
                       onChange={(e)=>(setSuburb(e.target.value))}></input>
                     </div>  
                   </div> 
@@ -78,14 +79,14 @@ const Register = () => {
                   <div className='col-md-5 mb-4 pb-2'>
                     <div className="form-outline form-white">
                       <label style={{color:"black"}} className="form-label label-style">Phone Number</label>
-                      <input type="text" className="form-control form-control-lg" value={phone} placeholder="Phone Number"
+                      <input type="text" className="form-control form-control-md" value={phone} placeholder="Phone Number"
                       onChange={(e)=>(setPhoneNumb(e.target.value))}></input>
                     </div>  
                   </div> 
                   <div className='col-md-7 mb-4 pb-2'>
                     <div className="form-outline form-white">
                       <label style={{color:"black"}} className="form-label label-style">Email Address</label>
-                      <input type="text" className="form-control form-control-lg" value={email} placeholder="Email Address"
+                      <input type="text" className="form-control form-control-md" value={email} placeholder="Email Address"
                       onChange={(e)=>(setEmail(e.target.value))}></input>
                     </div>  
                   </div> 
@@ -95,30 +96,22 @@ const Register = () => {
                   <div className='col-md-5 mb-4 pb-2'>
                     <div className="form-outline form-white">
                       <label style={{color:"black"}} className="form-label label-style" >User Name</label>
-                      <input type="text" className="form-control form-control-lg" value={username} placeholder="User Name"
+                      <input type="text" className="form-control form-control-md" value={username} placeholder="User Name"
                       onChange={(e)=>(setUserName(e.target.value))}></input>
                     </div>  
                   </div> 
                   <div className='col-md-7 mb-4 pb-2'>
                     <div className="form-outline form-white">
                       <label style={{color:"black"}} className="form-label label-style">Password</label>
-                      <input type="password" className="form-control form-control-lg" value={password} placeholder="Password"
+                      <input type="password" className="form-control form-control-md" value={password} placeholder="Password"
                       onChange={(e)=>(setPassword(e.target.value))}></input>
                     </div>  
                   </div> 
                 </div>
-
-                <div className="form-check mb-4 pb-3">
-                  <div>
-                    <input className="form-check-input" style={{float: "none", marginRight: 10 + "px"}} type="checkbox"></input>
-                    <label className='form-check-label' style={{color:"black"}}>
-                      I do accept the <a href="#" style={{color:"black"}}><u>Terms and Conditions</u></a> of your site
-                    </label>
-                  </div>
-                </div>
-                
-                <button type="button" className="btn btn-success">Create</button>
-                  
+                {/* let imagine success register will lead to signIn page*/}
+                <Link to="/SignIn">
+                  <button type="button" className="btn btn-success">Create</button> 
+                </Link>
                 
               </div>
             </div>
