0684accd904eecf11dcf51a32e034eab4690b9bc
Duy Quoc Anh Nguyen <boyporo2017@gmail.com>
2024-04-21 09:35:22 +0930

    Move course page to route folder

--- my-app/src/routes/index.js
+++ my-app/src/routes/index.js
@@ -5,4 +5,5 @@ export { default as SignIn } from './SignIn'
 export { default as SignInAsStaff } from './SignInAsStaff'
 export {default as Student_Home} from './Student_Home'
 export {default as Staff_Home} from './Staff_Home'
-export {default as Supervisor_Details}from './Supervisor_Details'
+export { default as Supervisor_Details } from './Supervisor_Details'
+export { default as CoursePage } from "./CoursePage";
