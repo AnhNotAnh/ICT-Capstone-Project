29a5d7dd9c3da1d436db6f453b73710ff490d141
rijin1 <rijyy001@mymail.unisa.edu.au>
2024-04-23 09:24:02 +0930

    Made the 3 buttons for logbook options a bit more prettier to match with the unisa colour and aesthetic.

--- my-app/src/routes/Student_Home.js
+++ my-app/src/routes/Student_Home.js
@@ -1,21 +1,37 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Student_Home = () => {
-  return (
-    <div>
-      <h2>Home Page</h2>
-      <p>Please select your logbook</p>
-        <button>General Logbook</button>
-        <Link to={"/StudentHome/CoursePage"} ><button>Cardiac Logbook</button></Link>
-        <button>Vascular Logbook</button>
-        <Link to="/Supervisor_Details">
-          <button style={{ position: 'absolute', top: '10px', right: '10px' }}>Supervisor Details</button>
-        </Link>
-      
-    </div>
+    const buttonStyle = {
+        padding: '10px 20px',
+        fontSize: '1.2em',
+        borderRadius: '5px',
+        margin: '5px',
+        background: 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
+        color: 'white',
+        border: 'none',
+        cursor: 'pointer',
+        boxShadow: '0 3px 5px rgba(0,0,0,0.3)',
+        textDecoration: 'none'
+    };
 
-  );
+const underLine = {
+      textDecoration: 'underline'
+};   
+
+
+    return (
+        <div>
+            <h2>Home Page</h2>
+            <p style={underLine}>Please select your logbook</p>
+            <button style={buttonStyle}>General Logbook</button>
+            <Link to={"/StudentHome/CoursePage"} style={{ textDecoration: 'none' }}><button style={buttonStyle}>Cardiac Logbook</button></Link>
+            <button style={buttonStyle}>Vascular Logbook</button>
+            <Link to="/Supervisor_Details">
+                <button style={{position:'absolute',top:'40px',right:'10px' }}>Supervisor Details</button>
+            </Link>
+        </div>
+    );
 };
 
-export default Student_Home
+export default Student_Home;
