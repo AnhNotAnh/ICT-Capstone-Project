fb3b9fd77c418e72988794d8d7f64503d9d62846
Duy Quoc Anh Nguyen <boyporo2017@gmail.com>
2024-05-04 11:29:54 +0930

    Fix title of course page

--- my-app/src/routes/CoursePage.js
+++ my-app/src/routes/CoursePage.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const CoursePage = () => {
     return (
     <div className="container text-center">
-      <h2 style={{ padding: "30px" }}>Cardiac Course Page</h2>
+      <h2 style={{ padding: "30px" }}>Cardiac Logbook</h2>
       <div className="row justify-content-center">
         {/* <div className="col-md-4 align-self-center"> */}
         <div className="col-md-4">
